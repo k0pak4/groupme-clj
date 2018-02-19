c0b3124570d3d1b8b7e2cbce43756f7dda175d41
Matthew Dunn <mdunn@cra.com>
2018-02-19 09:39:53 -0500

    Add enable/disable sms mode for users

--- src/groupme_clj/users.clj
+++ src/groupme_clj/users.clj
@@ -23,4 +23,27 @@
   (let [request (util/build-request "/users/update" token)
         body (build-body av-url name email zipcode)
         resp (util/make-request request "POST" body)]
-    (util/extract-content resp)))
+    (util/extract-content resp))) 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;; SMS Mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn enable-sms-mode
+  "Enable SMS mode for the authenticated user for the given duration in hours.
+  duration must be between 0 and 48 hours"
+  [token duration]
+  {:pre [(and (> duration 0) (<= duration 48))]}
+  (let [request (util/build-request "/users/sms_mode" token)
+        body {"duration" duration}
+        resp (util/make-request request "POST" body)]
+    (if (= (:status resp) 201)
+      (print "Successly enabled SMS Mode!"))))
+
+(defn disable-sms-mode
+  "Disable SMS mode for the authenticated user"
+  [token]
+  (let [request (util/build-request "/users/sms_mode/delete" token)
+        resp (util/make-request request "POST")]
+    (if (= (:status resp) 200)
+      (print "Successly disabled SMS Mode!"))))
