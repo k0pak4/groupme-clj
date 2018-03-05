9d9ff11b7e769f1d7044729a921eaa2fceae44f1
Matthew Dunn <mdunn@cra.com>
2018-03-05 09:28:04 -0500

    add tests for utility functions, changing build request

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.7.0"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [se.haleby/stub-http "0.2.4"]])
